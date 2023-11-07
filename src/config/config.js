08dce331d6cc40077438a9852cbe937581c8102f
ZamIzzudin <azzamizzudinhasan@gmail.com>
2023-11-07 17:23:57 +0700

    fix:(adjust env config to process.env,)

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,15 @@
-const dotenv = require('dotenv');
+require('dotenv').config()
 
-const config = dotenv.config().parsed
+const config = {
+    "PORT": process.env.PORT,
+    "MONGO_URL": process.env.MONGO_URL,
+    "MAX_AGE_ACCESS_TOKEN": process.env.MAX_AGE_ACCESS_TOKEN,
+    "MAX_AGE_REFRESH_TOKEN": process.env.MAX_AGE_REFRESH_TOKEN,
+    "REFRESH_TOKEN": process.env.REFRESH_TOKEN,
+    "ACCESS_TOKEN": process.env.ACCESS_TOKEN,
+    "REDIRECT_URI": process.env.REDIRECT_URI,
+    "CLIENT_ID": process.env.CLIENT_ID,
+    "CLIENT_SECRET": process.env.CLIENT_SECRET,
+}
 
 module.exports = config
