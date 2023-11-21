512a69aaecd77bc864c8b1f9f159783013f3563a
ZamIzzudin <azzamizzudinhasan@gmail.com>
2023-11-21 16:34:59 +0700

    fix:(try another crons setup)

--- src/crons/refreshGoogleToken.js
+++ src/crons/refreshGoogleToken.js
@@ -3,14 +3,14 @@ const connector = require('../config/gdrive.js')
 
 const { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI } = config
 
-const refresh = async (req, res) => {
+module.exports = async (req, res) => {
     try {
         const { refreshToken } = await connector(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
 
         const token = refreshToken()
 
         if (token) {
-            return res.status(200).json({
+            return res.status(200).send({
                 status: 200,
                 message: 'success renew token'
             })
@@ -19,12 +19,10 @@ const refresh = async (req, res) => {
         }
     } catch (err) {
         console.error(err.message)
-        return res.status(404).json({
+        return res.status(404).send({
             status: 404,
             message: 'Server Failed',
             info: 'token not found'
         })
     }
 }
-
-module.exports = refresh
