4eeae319825115a73a57a23d4caf79d8bdba3217
ZamIzzudin <azzamizzudinhasan@gmail.com>
2023-11-21 15:36:55 +0700

    debug:(rollback all changes)

--- src/routes/gauth.js
+++ src/routes/gauth.js
@@ -1,14 +1,6 @@
 const gauth = require('express').Router()
 const { auth, redirect, driveList, upload, webView, download, remove } = require('../controllers/gauth')
 
-// const config = require('../config/config.js')
-// const connector = require('../config/gdrive.js')
-// const { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI } = config
-
-// const { refreshToken } = connector(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
-
-// const cron = require('node-cron')
-
 // AUTH
 gauth.get('/auth', auth)
 gauth.get('/redirect', redirect)
@@ -24,9 +16,6 @@ gauth.post('/upload/:id_folder', upload)
 // DELETE
 gauth.delete('/delete/:id_file', remove)
 
-// cron.schedule('*/10 * * * *', () => {
-//     refreshToken();
-// });
 
 
 module.exports = gauth
