5576a43b63a31701eda82b176667aabf194cc6a0
Azzam Izzudin Hasan <88259831+ZamIzzudin@users.noreply.github.com>
2023-12-17 01:22:32 +0700

    fix:(bug on offer list controlller)

--- src/controllers/offer.js
+++ src/controllers/offer.js
@@ -3,7 +3,7 @@ const { getLatestNumber, updateLatestNumber } = require('./increment.js')
 
 const offer_list = async (req, res) => {
     try {
-        const offers = await Offer.find(query)
+        const offers = await Offer.find()
         if (offers.length > 0 && offers !== null) {
             res.status(200).json({
                 status: 200,
