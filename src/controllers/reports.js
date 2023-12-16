fe8491f3e4607cf91991fa006e76c5d409e58d63
ZamIzzudin <azzamizzudinhasan@gmail.com>
2023-12-16 23:25:38 +0700

    feat:(set tec on top rank revanue report)

--- src/controllers/reports.js
+++ src/controllers/reports.js
@@ -8,18 +8,37 @@ const get_report_per_month = async (req, res) => {
 
         if (reports) {
             // Sort
-            const top_percentage_rank = reports.detail.sort((curr, prev) => prev.percentage - curr.percentage).slice(0, 4).map((item => { return { category: item.category, sub_category: item.sub_category, percentage: item.percentage, unit: item.unit, revanue: item.revanue } }))
-            const top_revanue_rank = reports.detail.sort((curr, prev) => prev.revanue - curr.revanue).slice(0, 4).map((item => { return { category: item.category, sub_category: item.sub_category, percentage: item.percentage, unit: item.unit, revanue: item.revanue } }))
-            const top_unit_rank = reports.detail.sort((curr, prev) => prev.unit - curr.unit).slice(0, 4).map((item => { return { category: item.category, sub_category: item.sub_category, percentage: item.percentage, unit: item.unit, revanue: item.revanue } }))
+            const top_revanue_rank = reports.detail.sort((curr, prev) => prev.revanue - curr.revanue).slice(0, 3).map((item => { return { category: item.category, sub_category: item.sub_category, percentage: item.percentage, unit: parseInt(item.unit), revanue: item.revanue } }))
+
+            if (reports.detail.length > 3) {
+                let etc_percentage = 100
+                let etc_revanue = reports.total_revanue
+                let etc_unit = reports.total_unit
+
+                // Set ETC Data
+                top_revanue_rank.forEach(item => {
+                    etc_percentage -= item.percentage
+                    etc_revanue -= item.revanue
+                    etc_unit -= item.unit
+                })
+
+                const etc = {
+                    category: 'ETC',
+                    sub_category: 'ETC',
+                    percentage: etc_percentage,
+                    unit: etc_unit,
+                    revanue: etc_revanue
+                }
+
+                top_revanue_rank.push(etc)
+            }
 
             res.status(200).json({
                 bulan: reports.bulan,
                 tahun: reports.tahun,
                 total_revanue: reports.total_revanue,
                 total_unit: reports.total_unit,
-                top_percentage_rank,
                 top_revanue_rank,
-                top_unit_rank,
                 details: reports.detail
             })
         } else[
@@ -46,6 +65,7 @@ const get_report_per_month = async (req, res) => {
     }
 }
 
+// Gak Kepake Harusnya
 const get_report_per_year = async (req, res) => {
     const { year } = req.params
 
@@ -62,8 +82,8 @@ const get_report_per_year = async (req, res) => {
             })
 
             // Sort
-            const top_revanue_rank = reports.sort((curr, prev) => prev.total_revanue - curr.total_revanue).slice(0, 4).map((item => { return { bulan: item.bulan, total_revanue: item.total_revanue, total_unit: item.total_unit } }))
-            const top_unit_rank = reports.sort((curr, prev) => prev.total_unit - curr.total_unit).slice(0, 4).map((item => { return { bulan: item.bulan, total_revanue: item.total_revanue, total_unit: item.total_unit } }))
+            const top_revanue_rank = reports.sort((curr, prev) => prev.total_revanue - curr.total_revanue).slice(0, 5).map((item => { return { bulan: item.bulan, total_revanue: item.total_revanue, total_unit: item.total_unit } }))
+            const top_unit_rank = reports.sort((curr, prev) => prev.total_unit - curr.total_unit).slice(0, 5).map((item => { return { bulan: item.bulan, total_revanue: item.total_revanue, total_unit: item.total_unit } }))
 
             res.status(200).json({
                 status: 200,
